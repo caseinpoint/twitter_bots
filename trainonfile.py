1ae469a72ea52c7dd6adedc4aac39ad0b073d943
Drue Gilbert <drue.gilbert@gmail.com>
2020-06-17 18:00:06 -0700

    retrained all without weight adjustments

--- trainonfile.py
+++ trainonfile.py
@@ -3,18 +3,16 @@ from markov_algorithms import *
 
 chain = MarkovChain()
 
-# chain.train_on_file(filename='training_txt/end_of_faith.txt', verbose=True)
-# chain.train_on_file(filename='training_txt/God_Delusion.txt', verbose=True)
-# chain.train_on_file(filename='training_txt/god_is_not_great.txt', verbose=True)
-# chain.train_on_file(filename='training_txt/new_testament.csv', verbose=True)
-chain.train_on_file(filename='training_txt/quran.txt', verbose=True)
+chain.train_on_file(filename='training_txt/metahuman.txt', verbose=True)
+chain.train_on_file(filename='training_txt/waking_up.txt', verbose=True)
+
 
 print(f'len(chain.tree): {len(chain.tree)}\n')
 
 # print('Adjusting weights. This may take a while.\n_\n')
 # chain.bulk_adjust_weights(fitness_functions=[aw_mult(aw_favor_complexity, .001), aw_mult(aw_favor_punctuation, .00015), aw_mult(aw_favor_alternating_complexity, .1)], iterations=len(chain.tree))
 
-chain.save_training('bin/quran.bin')
+chain.save_training('bin/waking_meta.bin')
 
 for i in range(8):
 	print(chain.generate_tweet(append_tag=None, follow=False), '\n_\n')
